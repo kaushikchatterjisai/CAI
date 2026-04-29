61ba26403a2865efa08911a096ac5698c9ff3d08
kaushikchatterjisai <74865141+kaushikchatterjisai@users.noreply.github.com>
2026-04-29 08:26:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,12 +58,12 @@ resource "aws_eks_access_policy_association" "jenkins_admin" {
   }
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
- # encryption_config {
-  #  resources = ["secrets"]
-   # provider {
-    #  key_arn = data.aws_kms_key.eks.arn
-    #}
-  #}
+ encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = data.aws_kms_key.eks.arn
+    }
+  }
 }
 
 # Fetch default subnets
@@ -73,9 +73,9 @@ data "aws_subnets" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
-data "aws_kms_key" "eks" {
-  key_id = "alias/aws/ebs"
-}
+#data "aws_kms_key" "eks" {
+ # key_id = "alias/aws/ebs"
+#}
 
 # Fetch default VPC
 data "aws_vpc" "default" {
