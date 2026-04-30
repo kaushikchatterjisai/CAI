bb2a9f0762a18b8bb07cea00fdb613c86a018b4d
unknown <kaushikchatterjisai@gmail.com>
2026-04-30 08:43:46 +0530

    added the encryption key in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,9 +83,9 @@ data "aws_subnets" "default" {
     values = [data.aws_vpc.default.id]
   }
 }
-#data "aws_kms_key" "eks" {
- # key_id = "alias/aws/ebs"
-#}
+data "aws_kms_key" "existing_eks_key" {
+   key_id = "alias/eks-key-alias-here"
+   }
 
 # Fetch default VPC
 data "aws_vpc" "default" {
