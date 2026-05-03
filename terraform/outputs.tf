371337149c43f0bb036e95ceea22be66bc4c2d77
kaushikchatterjisai <74865141+kaushikchatterjisai@users.noreply.github.com>
2026-05-03 21:11:01 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
-output "public_ip" {
+# Replace the contents of outputs_bkp.tf with this:
+output "instance_public_ip" {
   value = aws_instance.ec2.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.ec2.id
 }
