6971c23fdfbc1b3a3e1362896757a19ba73494a4
kaushikchatterjisai <74865141+kaushikchatterjisai@users.noreply.github.com>
2026-05-04 00:25:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,20 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    description = "Allow outbound HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow outbound HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 # 2. IAM Role
