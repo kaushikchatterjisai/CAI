3410bf83cf85759d07c50af82eab5a099c5dab39
kaushikchatterjisai <74865141+kaushikchatterjisai@users.noreply.github.com>
2026-05-04 00:29:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,7 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    description = "Allow outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
+   egress {
     description = "Allow outbound HTTP"
     from_port   = 80
     to_port     = 80
