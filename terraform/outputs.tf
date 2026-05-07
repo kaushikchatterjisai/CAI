cc4b2aaa70574c78ff7491ce127d62eaaa91a614
kaushikchatterjisai <74865141+kaushikchatterjisai@users.noreply.github.com>
2026-05-07 07:09:24 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 # Replace the contents of outputs_bkp.tf with this:
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value = aws_instance.Capstone-App.public_ip # Ensure this matches your resource name
+  value = aws_instance.ec2.public_ip # Ensure this matches your resource name
 }
 
 output "instance_id" {
