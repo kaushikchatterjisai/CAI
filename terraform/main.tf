3ce29b7d8e79f8cee6e7ce21e31827e3b9a9dc5a
kaushikchatterjisai <74865141+kaushikchatterjisai@users.noreply.github.com>
2026-05-07 06:38:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
-  associate_public_ip_address = true
+  
   monitoring    = true
   ebs_optimized = true
 
